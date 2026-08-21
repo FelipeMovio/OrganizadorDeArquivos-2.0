ebcfd058cef12e5735654540189c8ab9acf2fc6a
Felipe Movio <felipelsmovio@gmail.com>
2026-08-21 13:33:44 -0300

    ExtensaoArquivoUtilClass

--- OrganizadorDePasta-2.0/Utils/ExtensaoArquivoUtil.cs
+++ OrganizadorDePasta-2.0/Utils/ExtensaoArquivoUtil.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace OrganizadorDePasta_2._0.Utils;
 
-public class ExtensaoArquivoUtil
+public static class ExtensaoArquivoUtil
 {
+    public static string ObterExtensao(string caminhoArquivo)
+    {
+        return Path.GetExtension(caminhoArquivo).ToLowerInvariant();
+    }
 }
