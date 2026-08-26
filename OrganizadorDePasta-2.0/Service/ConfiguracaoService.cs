c2ae2eb6f72a53996d4a3a8ce9613245bb539118
Felipe Movio <felipelsmovio@gmail.com>
2026-08-25 22:07:04 -0300

    create/ configuracaoService , onde ocorre a deserilizacao do confi.json para o objeto

---
+++ OrganizadorDePasta-2.0/Service/ConfiguracaoService.cs
@@ -0,0 +1,21 @@
+﻿
+using OrganizadorDePasta_2._0.Models;
+using System.IO;
+using System.Text.Json;
+namespace OrganizadorDePasta_2._0.Service;
+
+public class ConfiguracaoService
+{
+    public Configuracao CarregarConfiguracao()
+    {
+        // Ler todo conteudo do arquivo
+        var json = File.ReadAllText("config.json");
+
+        // Desserializa o JSON de volta para o objeto 
+        var objeto = JsonSerializer.Deserialize<Configuracao>(json);
+
+        //O ! é o null-forgiving operator. Estamos dizendo ao compilador
+        //Neste ponto, eu estou assumindo que esse objeto não será null
+        return objeto!;
+    }
+}
