5956bd2caf47af5da36e58f8f044f6a62260d02e
Felipe Movio <felipelsmovio@gmail.com>
2026-08-28 22:50:30 -0300

    create class monitoramentoService

---
+++ OrganizadorDePasta-2.0/Service/MonitoramentoService.cs
@@ -0,0 +1,20 @@
+﻿
+using System.IO;
+
+
+namespace OrganizadorDePasta_2._0.Service;
+
+public class MonitoramentoService
+{
+    private readonly FileSystemWatcher _watcher;
+
+    public MonitoramentoService(OrganizadorService organizadorService)
+    {
+        _watcher = new FileSystemWatcher();
+
+        _watcher.Created += (sender, e) =>
+        {
+            Console.WriteLine($"Novo arquivo detectado: {e.FullPath}");
+        };
+    }
+}
